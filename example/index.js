1177ac4e949be4927c7d6c6839036a481989624d
Doron Segal <doron2402@gmail.com>
2014-06-14 19:25:11 -0700

    improving

--- example/index.js
+++ example/index.js
@@ -2,21 +2,19 @@ var SmartHash = require('smarthash').SmartHash;
 
 
 var h = new SmartHash();
-h.insert({index: 'doron', value: {phone_num: '1'}, ttl: 200});
-h.insert({index: 'doron', value: {phone_num: '2'}, ttl: 200});
-h.insert({index: 'doron', value: {phone_num: '3'}, ttl: 200});
-h.insert({index: 'doron', value: {phone_num: '4'}, ttl: 200});
-h.insert({index: 'rachel', value: {phone_num: '5'}, ttl: 200});
-h.insert({index: 'rachel', value: {phone_num: '6'}, ttl: 200});
+h.insert({index: 'doron', value: {phone_num: '1'}, ttl: 3200});
+h.insert({index: 'doron', value: {phone_num: '2'}, ttl: 3200});
+h.insert({index: 'doron', value: {phone_num: '3'}, ttl: 33000});
+h.insert({index: 'doron', value: {phone_num: '4'}, ttl: 23000});
+h.insert({index: 'rachel', value: {phone_num: '5'}, ttl: 32200});
+h.insert({index: 'rachel', value: {phone_num: '6'}, ttl: 53000});
 console.log(h.fetch({index: 'doron'}));
 console.log(h.fetch({index: 'rachel'}));
 console.log(h.Data);
-setTimeout(function(){
+var recursive = function () {
+    console.log("It has been one second!");
     console.log(h.Data);
-}, 100);
-setTimeout(function(){
-    console.log(h.Data);
-}, 400);
-setTimeout(function(){
-    console.log(h.Data);
-}, 800);
+    console.log(h.fetch({index: 'doron'}));
+    setTimeout(recursive,200);
+}
+recursive();
